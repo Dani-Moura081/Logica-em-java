84bbd55d6b4d18866b384a231d121d87e9012174
Dani-Moura081 <dani.fe.olav.rodrigues@gmail.com>
2025-02-12 21:14:58 -0300

    conclusão da aula 3 de lógica

--- aula03.java
+++ aula03.java
@@ -21,8 +21,61 @@ public class aula03 {
         System.out.println("em caso de divisão:");
         System.out.println(num1 / num2);
         System.out.println("em caso de resto:");
-        System.out.println(num1 % num2);
+        System.out.println(num1 % num2 );
 
+
+
+        // operadores de atribuição : = , += , -= , *= , /= , %=
+        System.out.println("diga um número: ");    
+        int x = sc.nextInt();
+        System.out.println("quando o operador é = :");
+        System.out.println(x);        
+
+        x += 8; // x = x + 8
+        System.out.println("quando o operador é += : ");
+        System.out.println(x ); 
         
+        x -= 5; // x = x - 5
+        System.out.println("quando o operador é -= :");
+        System.out.println(x); 
+        
+        x *= 2; // x = x * 2
+        System.out.println("quando o operador é *= :");
+        System.out.println(x); 
+
+        x /= 2; // x = x / 2
+        System.out.println("quando o operador é /= :");
+        System.out.println(x); 
+        x %= 3; // x = x % 3
+        System.out.println("quando o operador é %= :");
+        System.out.println(x); 
+        // operadores de comparação : == , != , > , < , >= , <=
+        System.out.println("digite um número: ");
+        int y = sc.nextInt();
+        System.out.println("quando o operador é == :");
+        System.out.println(y == 5); // true ou false
+        System.out.println("quando o operador é != :");
+        System.out.println(y != 5); // true ou false
+        System.out.println("quando o operador é > :");
+        System.out.println(y > 5); // true ou false
+        System.out.println("quando o operador é < :");
+        System.out.println(y < 5); // true ou false
+        System.out.println("quando o operador é >= :");
+        System.out.println(y >= 5); // true ou false
+        System.out.println("quando o operador é <= :");
+        System.out.println(y <= 5); // true ou false
+        // operadores lógicos : && , || , !, ^
+        System.out.println("digite um número: ");
+        int z = sc.nextInt();
+        System.out.println("quando o operador é && :");
+        System.out.println(z > 5 && z < 10); // true ou false
+        System.out.println("quando o operador é || :");
+        System.out.println(z > 5 || z < 10); // true ou false
+        System.out.println("quando o operador é ! :");
+        System.out.println(! (z > 5 && z < 10)); // true ou false
+        System.out.println("quando o operador é ^ :");
+        System.out.println(z > 5 ^ z < 10); // true ou false
+
+
     }
 }
